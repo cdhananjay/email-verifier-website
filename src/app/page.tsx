0d88df553e997ef3bd138055cd77207e4e6ac4ca
Dhananjay Chavan <219833325+cdhananjay@users.noreply.github.com>
2026-06-22 22:19:18 +0530

    feat: setup magic link signin and linking discord
    - use resend for sending emails

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,41 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
+
 export default function Home() {
-  return <h1 className="text-red-400">hello world</h1>;
+  async function signIn() {
+    const { data: _data, error: _error } = await authClient.signIn.magicLink({
+      email: "test@example.com", // required
+      name: "my-name",
+      callbackURL: "/dashboard",
+      newUserCallbackURL: "/welcome",
+      errorCallbackURL: "/error",
+      metadata: { inviteId: "123" },
+    });
+  }
+  async function linkDiscord() {
+    await authClient.linkSocial({
+      provider: "discord",
+    });
+  }
+  const {
+    data: session,
+    isPending, //loading state
+    error, //error object
+    refetch: _refetch, //refetch the session
+  } = authClient.useSession();
+
+  if (isPending) return <h1>loading..</h1>;
+  if (error) return <h1>error</h1>;
+
+  return (
+    <>
+      <h1>{JSON.stringify(session?.user)}</h1>
+      <button type="button" className="border" onClick={signIn}>
+        signin
+      </button>
+      <button type="button" className="border" onClick={linkDiscord}>
+        link discord
+      </button>
+    </>
+  );
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,37 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { magicLink } from "better-auth/plugins";
+import { resend } from "./email";
 import { prisma } from "./prisma";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  account: {
+    accountLinking: {
+      allowDifferentEmails: true,
+    },
+  },
+  plugins: [
+    magicLink({
+      sendMagicLink: async (
+        { email, token: _token, url, metadata: _metadata },
+        _ctx,
+      ) => {
+        resend.emails.send({
+          from: "onboarding@resend.dev",
+          to: email,
+          subject: "Your login link.",
+          text: `login with this link: ${url}`,
+        });
+      },
+    }),
+  ],
+  socialProviders: {
+    discord: {
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    },
+  },
 });
